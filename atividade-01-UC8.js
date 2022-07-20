8e161e4291b1bd11859fe4334741d652df3a5d0c
dpalves19 <dionatanalves19@gmail.com>
2022-07-20 18:08:23 -0300

    atividade-1 UC8

---
+++ atividade-01-UC8.js
@@ -0,0 +1,15 @@
+//Requisitos para o programa 
+
+//Se a peça possuir um peso superior a 100 gramas, pode cadastrar
+var listaDePecas = ["amortecedor", "Motor", "Filtro de Ar"]
+
+let peso = 50;
+if(peso<100){
+    console.log("A peça deve pesar no minimo 100g")
+}
+
+//- Dada a capacidade de cada caixa, caso a lista de peças seja superior a 10, imprima uma mensagem informando não ter capacidade suficiente.
+
+
+
+//- Caso a peça tenha um nome com quantidade inferior a 3 caracteres, informe uma mensagem de erro. 
